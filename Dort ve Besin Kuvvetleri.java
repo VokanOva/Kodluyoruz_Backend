393442f93363695c5f594e53b54a54bca8b20478
Mahmoud ALHAMWIEH <102381976+VokanOva@users.noreply.github.com>
2022-04-17 12:27:19 +0300

    Create Dort ve Besin Kuvvetleri.java

---
+++ Dort ve Besin Kuvvetleri.java
@@ -0,0 +1,14 @@
+public class java {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 80; i *= 5) {
+            System.out.println(i);
+        }
+
+        for  (int k =1; k<=100; k*=4){
+           System.out.println(k);
+
+
+       }
+    }
+}
